52362086b1d76b02c00d39b05a904b245615ed70
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2024-06-29 03:10:39 +0300

    stuff- built the model but not tested it yet

--- translate/model.py
+++ translate/model.py
@@ -1,10 +1,29 @@
 from dataclasses import dataclass
+import os
+import math
 import torch
 from torch import nn
 import torch.nn.functional as F
 
+@dataclass
+class T5Config:
+    block_size: int = 1024
+    vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    n_layer: int = 12 # number of layers - for both encoder and decoder blocks
+    n_head: int = 12
+    n_embd: int = 768
+    dropout: float = 0.0
+    bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    padding_idx: int = 0 # padding token index
+
 class LayerNorm(nn.Module):
-    """ LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False """
+    """ 
+    LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False 
+
+    Paper: https://arxiv.org/pdf/2002.04745
+    It proposes that pre-LN stabizes the training process and leads to faster convergence.
+    """
+
 
     def __init__(self, ndim, bias):
         super().__init__()
@@ -17,7 +36,7 @@ class LayerNorm(nn.Module):
 class MLP(nn.Module):
     """ Simple Feed Forward Network with GELU activation. """
 
-    def __init__(self, config):
+    def __init__(self, config:T5Config):
         super().__init__()
         # contextual fully connected.
         self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
@@ -34,29 +53,247 @@ class MLP(nn.Module):
         x = self.dropout(x)
         return x
 
-@dataclass
-class T5Config:
-    block_size: int = 1024
-    vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
-    n_layer: int = 12
-    n_head: int = 12
-    n_embd: int = 768
-    dropout: float = 0.0
-    bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
-class T5(nn.Module):
+class MultiheadAttention(nn.Module):
+    """
+    Multihead scaled dot-product attention with optional Flash Attention support.
+    """
+    def __init__(self, config):
+        super().__init__()
+
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch.
+        # combined dense layer is more efficient and simple
+        # silly reference: https://chatgpt.com/share/b6a00998-1151-45a9-ad96-310dcf7adeb6
+        self.c_attn = nn.Linear(3 * config.n_embd, 3 * config.n_embd, bias=config.bias)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        # regularization
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.dropout = config.dropout
+        self.block_size = config.block_size
+
+        # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
+        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
+        if not self.flash:
+            print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
+            # causal mask to ensure that attention is only applied to the left in the input sequence
+
+    def forward(self, queries, keys, values, mask=None):
+        B, T, C = queries.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        assert T == keys.size(1) == values.size(1) == self.block_size
+        assert self.n_embd == C
+
+        # NOTE: I am contemplating whethere regsiter_buffer is necessary or not.
+        if mask is None:
+            self.register_buffer("bias", torch.tril(torch.ones(self.block_size, self.block_size))
+                                        .view(1, 1, self.block_size, self.block_size))
+        else:
+            self.register_buffer("bias", mask)
+
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        q, k, v  = self.c_attn(torch.cat((queries, keys, values), dim=2)).split(self.n_embd, dim=2)
+
+
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+
+        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        if self.flash:
+            # efficient attention using Flash Attention CUDA kernels
+            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=self.bias, dropout_p=self.dropout if self.training else 0, is_causal=True)
+        else:
+            # manual implementation of attention
+            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+            att = F.softmax(att, dim=-1)
+            y = self.attn_dropout(att)
+            y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+        # output projection
+        y = self.resid_dropout(self.c_proj(y))
+
+        #NOTE: Research on how to get the attention weights from flash attention.
+        return y
+
+class EncoderBlock(nn.Module):
     def __init__(self, config: T5Config):
-        self.config = config
+        super().__init__()
+        # as said previously, pre-LN is better than post-LN.
+        self.ln_1 = LayerNorm(config.n_embd, config.bias)
+        self.attn = MultiheadAttention(config)
+        self.ln_2 = LayerNorm(config.n_embd, config.bias)
+        self.mlp = MLP(config)
 
-    def forward(self, input_ids, attention_mask):
-        pass
+    def forward(self, x, mask):
+        # why an input mask is necessary?
+        # in translation tasks, we need to mask out the padding tokens
+        x = x + self.attn(self.ln_1(x), mask)
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+class Encoder(nn.Module):
+    def __init__(self, config: T5Config):
+        super().__init__()
+        self.config = config
+        self.encoder = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            drop = nn.Dropout(config.dropout),
+            h = nn.ModuleList([EncoderBlock(config) for _ in range(config.n_layer)]),
+            ln_f = LayerNorm(config.n_embd, bias=config.bias),
+        ))
+
+    def forward(self, input_ids, input_mask):
+        t = input_ids.size(1)
+        device = input_ids.device
+
+        assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
+        self.coefficient = torch.sqrt(torch.FloatTensor([self.config.n_embd], device=device)) # is this necessary? since its a leaf tensor
+        pos = torch.arange(0, t, dtype=torch.long, device=device) # shape (t)
+        
+        y = self.dp((self.wte(input_ids) * self.coefficient) + self.wpe(pos))
+
+        for layer in self.encoder.h:
+            y = layer(y, input_mask)
+
+        y = self.encoder.ln_f(y)
+        return y
+
+class DecoderBlock(nn.Module):
+    def __init__(self, config: T5Config):
+        super().__init__()
+        # as said previously, pre-LN is better than post-LN.
+        self.ln_1 = LayerNorm(config.n_embd, config.bias)
+        self.attn = MultiheadAttention(config)
+        self.ln_2 = LayerNorm(config.n_embd, config.bias)
+        self.attn_2 = MultiheadAttention(config)
+        self.mlp = MLP(config)
+        self.drop = nn.Dropout(config.dropout)
+
+    def forward(self, target, encoded_input, input_mask):
+        # why an input mask is necessary?
+        # in translation tasks, we need to mask out the padding tokens
+        # NOTE: This is a slight deviation from the original implementation.
+        # in here, target is passed into a self-attention layer, then passed into a cross-attention layer
+        target = self.ln_1(target)
+        target = target + self.drop(self.attn(target, target, target))
+        target = self.ln_2(target)
+        encoded_input = self.ln_2(encoded_input)
+        y = target + self.drop(self.attn(target, encoded_input, encoded_input, mask=input_mask))
+        y = self.drop(y)
+        y = y + self.drop(self.mlp(self.ln_2(y)))
+        return y
+
+class Decoder(nn.Module):
+    def __init__(self, config: T5Config):
+        super().__init__()
+        self.config = config
+        self.decoder = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            drop = nn.Dropout(config.dropout),
+            h = nn.ModuleList([DecoderBlock(config) for _ in range(config.n_layer)]),
+            ln_f = LayerNorm(config.n_embd, bias=config.bias),
+        ))
+
+    def forward(self, target, encoded_input, target_mask, input_mask):
+        T = target.size(1)
+        device = target.device
+
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+        self.coefficient = torch.sqrt(torch.FloatTensor([self.config.n_embd], device=device)) # is this necessary? since its a leaf tensor
+        pos = torch.arange(0, T, dtype=torch.long, device=device) # shape (t)
+        
+        target = self.dp((self.wte(target) * self.coefficient) + self.wpe(pos))
+
+        for layer in self.encoder.h:
+            target = layer(target, encoded_input, target_mask, input_mask)
+
+        target = self.encoder.ln_f(target)
+        return target
+
+class Transformer(nn.Module):
+    def __init__(self, encoder:Encoder, decoder:Decoder, config:T5Config, device, padding_idx=0):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+        self.padding_idx = padding_idx
+
+        self.apply(self._init_weights)
+
+        # apply special scaled init to the residual projections, per GPT-2 paper
+        for pn, p in self.named_parameters():
+            if pn.endswith('c_proj.weight'):
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+
+        # report number of parameters
+        print("total params: %.2fM\t\tnon-embedding params: %.2fM" % (self.get_num_params()/1e6,), (self.get_num_params(non_embedding=True)/1e6,))
+
+    def get_num_params(self, non_embedding=True):
+        """
+        Return the number of parameters in the model.
+        For non-embedding count (default), the position embeddings get subtracted.
+        The token embeddings would too, except due to the parameter sharing these
+        params are actually used as weights in the final layer, so we include them.
+        """
+        n_params = sum(p.numel() for p in self.parameters())
+        if non_embedding:
+            n_params -= self.encoder.encoder.wpe.weight.numel()
+            n_params -= self.decoder.decoder.wpe.weight.numel()
+        return n_params
+
+    def forward(self, input, target):
+        input_mask = (input != self.padding_idx).unsqueeze(1).unsqueeze(2)
+
+        #encoder feed through
+        encoded_input = self.encoder(input, input_mask)
+
+        #decoder feed_through
+        output = self.decoder(target, encoded_input, input_mask)
+
+        return output
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def save_pretrained(self, save_directory):
-        pass
+        #check if directory existso
+        if not os.path.exists(save_directory):
+            os.makedirs(save_directory)
+        #save config
+        self.config.save_pretrained(save_directory)
+        # save model
+        torch.save(self.state_dict(), os.path.join(save_directory, "pytorch_model.bin"))
+
 
     @classmethod
     def from_pretrained(cls, pretrained_model_name_or_path):
         pass
 
-    def generate(self, input_ids, attention_mask):
-        pass
+if __name__ == "__main__":
+    print("runnn")
+    config = T5Config()
+    print("runnn")
+    encoder = Encoder(config)
+    print("runnn")
+    decoder = Decoder(config)
+    print("runnn")
+    model = Transformer(encoder, decoder, config, device='cuda')
+    input = torch.randint(0, 50304, (1, 1024))
+    target = torch.randint(0, 50304, (1, 1024))
+    output = model(input, target)
+    print("runnn")
+    print(output.size())
+
